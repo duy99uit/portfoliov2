e96f19dc863d9da8850f2824124fcfac23e4a2ff
Doan The Duy <duy99.uit@gmail.com>
2022-09-27 14:35:14 +0700

    add loading full screen

--- src/components/loading/LoadingScreen.tsx
+++ src/components/loading/LoadingScreen.tsx
@@ -3,23 +3,16 @@ import React from "react";
 
 const LoadingScreen = () => {
   return (
-    <div className="h-screen w-screen fixed top-0 left-0 z-20  flex justify-center items-center">
-      {/* <p className="text-gray-300 text-base">Loading</p> */}
+    <div className="h-screen w-screen fixed top-0 left-0 z-20  flex justify-center items-center bg-black">
       <motion.div
-        className="box"
+        className="w-12 h-12 rounded-full bg-gradient-to-r from-purple-400 to-purple-700"
         animate={{
           scale: [1, 2, 2, 1, 1],
-          rotate: [0, 0, 180, 180, 0],
-          borderRadius: ["0%", "0%", "50%", "50%", "0%"],
+          rotate: [0, 0, 270, 270, 0],
+          borderRadius: ["20%", "20%", "50%", "50%", "20%"],
         }}
-        transition={{
-          duration: 2,
-          ease: "easeInOut",
-          times: [0, 0.2, 0.5, 0.8, 1],
-          repeat: Infinity,
-          repeatDelay: 1,
-        }}
-      />
+        transition={{ repeat: Infinity, duration: 2 }}
+      ></motion.div>
     </div>
   );
 };
--- src/container/Home/index.tsx
+++ src/container/Home/index.tsx
@@ -1,17 +1,31 @@
 import AboutSection from "@/components/AboutSection";
 import IntroSection from "@/components/IntroSection";
+import LoadingScreen from "@/components/loading/LoadingScreen";
 import NavBar from "@/components/NavBar";
 import ProjectSection from "@/components/ProjectSection";
 import SkillsSection from "@/components/SkillsSection";
+import React, { useState, useEffect } from "react";
 
 export default function HomeContainer() {
+  const [isLoadingPage, setIsLoadingPage] = useState<boolean>(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoadingPage(false);
+    }, 500);
+  }, []);
   return (
     <>
-      {/* <NavBar />
-      <IntroSection />
-      <AboutSection />
-      <SkillsSection /> */}
-      <ProjectSection />
+      {isLoadingPage ? (
+        <LoadingScreen />
+      ) : (
+        <>
+          <NavBar />
+          <IntroSection />
+          <AboutSection />
+          <SkillsSection />
+          <ProjectSection />
+        </>
+      )}
     </>
   );
 }
