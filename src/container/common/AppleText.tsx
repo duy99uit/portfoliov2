0e51ace1aa8ca36dc8c256a11f82b6562ae493d0
Doan The Duy <duy99.uit@gmail.com>
2023-06-21 00:04:46 +0700

    update code

--- src/container/common/AppleText.tsx
+++ src/container/common/AppleText.tsx
@@ -2,10 +2,12 @@ export const AppleText = (props: { text?: string; size?: number }) => {
   const { text } = props;
   return (
     <div className="inline-block relative">
-      <h1 className={`text-indigo-900 inline-block text-6xl font-bold`}>
+      <h1
+        className={`text-indigo-900 inline-block text-5xl sm:text-6xl font-bold`}
+      >
         {text}
         <span
-          className={`absolute top-0 left-0 w-full h-full bg-gradient-to-r from-purple-200 via-purple-600 to-indigo-900 bg-clip-text text-transparent text-6xl font-bold`}
+          className={`absolute top-0 left-0 w-full h-full bg-gradient-to-r from-purple-200 via-purple-600 to-indigo-900 bg-clip-text text-transparent text-5xl sm:text-6xl font-bold`}
         >
           {text}
         </span>
